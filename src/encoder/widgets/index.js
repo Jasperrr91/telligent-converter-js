2f656614db9efbd2b3324e2cabd6b796085738da
Jasper <jasper@vanderstoop.nl>
2020-01-23 16:20:38 +0100

    :pencil: More comments for code clarification

--- src/encoder/widgets/index.js
+++ src/encoder/widgets/index.js
@@ -14,18 +14,14 @@ export default function widgetEncoder(widgetDir) {
   const widgetOptionsFile = [widgetDir, '/widget_options.json'].join('');
   if (!(fs.existsSync(widgetOptionsFile))) return undefined;
 
-  const scriptedContentFragment = {}
-
-  const widgetOptions = fs.readFileSync(widgetOptionsFile, 'utf8');
-  const widgetOptionsJson = JSON.parse(widgetOptions);
-  scriptedContentFragment.attr = widgetOptionsJson;
-
+  // Initialize paths to files
   const contentScriptFile = [widgetDir, '/contentScript.vm'].join('');
   const headerScriptFile = [widgetDir, '/headerScript.vm'].join('');
   const configurationFile = [widgetDir, '/configuration.xml'].join('');
   const languageResourcesFile = [widgetDir, '/languageResources.xml'].join('');
   const additionalCssScriptFile = [widgetDir, '/additionalCssScript.css'].join('');
 
+  // Read and encode each element of the widget
   const contentScript = getVelocityScript(contentScriptFile);
   const headerScript = getVelocityScript(headerScriptFile);
   const configuration = getXMLFile(configurationFile);
@@ -33,6 +29,14 @@ export default function widgetEncoder(widgetDir) {
   const additionalCssScript = getVelocityScript(additionalCssScriptFile);
   const files = getAssetFiles(widgetDir);
 
+  // Read the widget options which contains the attributes for the widget tag
+  const widgetOptions = fs.readFileSync(widgetOptionsFile, 'utf8');
+  const widgetOptionsJson = JSON.parse(widgetOptions);
+  const scriptedContentFragment = {
+    attr: widgetOptionsJson,
+  };
+
+  // For each widget element, check of it exists and if so; add it to the widget object
   scriptedContentFragment.contentScript = contentScript;
   scriptedContentFragment.headerScript = headerScript;
   if (configuration) scriptedContentFragment.configuration = configuration;
