2c296b4a67e9a426d8c01598755edb301cb4eae2
Jasper <jasper@vanderstoop.nl>
2020-01-23 16:16:39 +0100

    :pencil: Added more comments for code clarification

--- src/encoder/themes/index.js
+++ src/encoder/themes/index.js
@@ -11,13 +11,14 @@ import {
 } from './functions';
 
 export function encodeTheme(themeFolder) {
+  // Initialize paths to files
   const headScriptFile = [themeFolder, '/scripts/headScript.vm'].join('');
   const bodyScriptFile = [themeFolder, '/scripts/bodyScript.vm'].join('');
   const configurationFile = [themeFolder, '/configuration.xml'].join('');
   const paletteTypesFile = [themeFolder, '/palette-types.xml'].join('');
   const languageResourcesFile = [themeFolder, '/language-resources.xml'].join('');
 
-  const themeOptions = getThemeOptions(themeFolder);
+  // Read and encode each element of the theme
   const headScript = getScript(headScriptFile);
   const bodyScript = getScript(bodyScriptFile);
   const configuration = getXMLFile(configurationFile);
@@ -29,10 +30,13 @@ export function encodeTheme(themeFolder) {
   const styleFiles = getStyleFiles(themeFolder);
   const pageLayouts = getPageLayouts(themeFolder);
 
+  // Read the theme options which contains the attributes for the theme tag
+  const themeOptions = getThemeOptions(themeFolder);
   const theme = {
     attr: themeOptions,
   };
 
+  // For each theme element, check of it exists and if so; add it to the theme object
   if (headScript) theme.headScript = headScript;
   if (bodyScript) theme.bodyScript = bodyScript;
   if (configuration) theme.configuration = configuration;
@@ -56,16 +60,17 @@ export function encodeTheme(themeFolder) {
  * @param {String} themeDir the dir containing the theme, or the themefolders if multiple
  */
 export default function themeEncoder(themeDir) {
-  // Some Theme XML files consist of a single theme
-  // Some consist of multiple themes
+  // Check if folder only contains one theme, then encode and return it
   const themeOptionsFile = [themeDir, '/theme_options.json'].join('');
   if (fs.existsSync(themeOptionsFile)) {
     return encodeTheme(themeDir);
   }
 
+  // Folder contains multiple themes, encode them into a single object
   const themes = [];
   const subDirectories = fs.readdirSync(themeDir);
 
+  // Make sure each folder indeed contains a theme, before encoding and adding it to the object
   subDirectories.forEach((subDir) => {
     const pathToSubDir = [themeDir, subDir].join('');
     const themeOptionsFile2 = [pathToSubDir, '/theme_options.json'].join('');
